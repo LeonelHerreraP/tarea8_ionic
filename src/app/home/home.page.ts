46659a377952c2e2a7db4f4a7b3c0a42e1cd97ba
Leonel Herrera <leonel48473@hotmail.es>
2022-11-08 12:35:31 -0400

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { RecordingData, VoiceRecorder } from 'capacitor-voice-recorder';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(private storage: Storage, public photoService: PhotoService) { }
+  
+  async ngOnInit() {
+    this.loadFiles();
+    VoiceRecorder.requestAudioRecordingPermission();
+  
+    await this.storage.create();
 
-  constructor() {}
+    this.cargarVivencias();
+  }
+
+  recording = false;
+  storedFileNames = [];
+  
+  // inputs modal
+  public titulo: string = "";
+  public descripcion: string = "";
+  public fecha: string = "";
+  vivencias = [];
+  
+  // Storage
+  async cargarVivencias(){
+    this.vivencias = [];
+    this.storage.forEach((key, value, index) => {
+      this.vivencias.unshift(key);
+    });
+  }
+
+  async addVivencia(){
+    this.fecha = "" + new Date().getDate()+ "-" + new Date().getMonth() + "-" + new Date().getFullYear();
+
+
+    await this.storage.set(this.titulo, {
+      'titulo': this.titulo,
+      'descripcion': this.descripcion,
+      'fecha': this.fecha
+    });
+    this.cargarVivencias();
+    this.setOpen(false);
+
+    this.titulo = "";
+    this.descripcion = "";
+  }
+
+  // CUESTION MODAL
+  isModalOpen = false;
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  // CUESTION FOTOS-CAMARA
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+
+  async loadFiles() {
+    Filesystem.readdir({
+      path: '',
+      directory: Directory.Data
+    }).then(result => {
+      console.log(result);
+      this.storedFileNames = result.files;
+    })
+  }
+
+  startRecording() {
+    if (this.recording) {
+      return;
+    }
+    this.recording = true;
+    VoiceRecorder.startRecording();
+  }
+
+  stopRecording() {
+    if (!this.recording) {
+      return;
+    }
+    VoiceRecorder.stopRecording().then(async (result: RecordingData) => {
+      if (result.value && result.value.recordDataBase64) {
+        const recordData = result.value.recordDataBase64;
+        const fileName = new Date().getTime() + '.wav';
+        await Filesystem.writeFile({
+          path: fileName,
+          directory: Directory.Data,
+          data: recordData
+        });
+        this.loadFiles();
+      }
+    })
+  }
 
 }
